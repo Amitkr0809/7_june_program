507e12a32984342959db277a91d95657a11d8d2c
Amit Kumar <125899008+Amitkr0809@users.noreply.github.com>
2023-06-07 14:42:12 +0530

    Add files via upload

---
+++ Oops_class_Object.py
@@ -0,0 +1,14 @@
+# create class
+class MyClass:
+  x = 5
+
+print(MyClass)
+
+
+
+# create Object
+class MyClass:
+  x = 5
+
+p1 = MyClass()   #>>  Object
+print(p1.x)
---
+++ Oops_init1.py
@@ -0,0 +1,9 @@
+class Mobile:
+    def __init__(self, model, storage):
+        self.model = model
+        self.storage = storage
+
+
+obj = Mobile("iPhone 12 Pro", "128GB")
+print(obj.storage)
+
